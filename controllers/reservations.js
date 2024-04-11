e225a557b7616d8a5109d33cff998ebae14e121d
Peeraphat Wongsri <peeraphat4106@gmail.com>
2024-04-11 18:06:14 +0700

    remove get,update,delete reservation due to bug

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -61,37 +61,7 @@ exports.addReservation = async (req, res, next) => {
 //@access Public
 
 exports.getReservations = async (req,res,next) =>{
-    let query;
-    //General users can see only thier reservations!
-    if(req.user.role !== 'admin'){
-        query = Reservation.find({user: req.user.id}).populate({
-            path: "massageShop",
-            select: "name address district province tel"
-        });
-    }else{ // iF you are admin, you can see all!
-
-        if(req.params.shopId){
-
-            console.log(req.params.shopId);
-
-            query = Reservation.find({shop: req.params.shopId}).populate({
-                path: "massageShop",
-                select: "name address district province tel",
-            });
-        }
-        else query = Reservation.find().populate({
-            path: "massageShop",
-            select: "name address district province tel",
-        });
-    }
-    try{
-        const reservations = await query;
-        res.status(200).json({success:true,count:reservations.length,data: reservations});
-
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({success:false,message:"Cannot find Reservation"});
-    }
+    return true;
 };
 
 
@@ -100,31 +70,7 @@ exports.getReservations = async (req,res,next) =>{
 //@access Private
 
 exports.updateReservation = async (req,res,next)=>{
-    try{
-        let reservation = await Reservation.findById(req.params.id);
-
-        if(!reservation){
-            return res.status(404).json({success:false,message:`No reservation with the id of ${req.params.id}`});
-        }
-
-        //Make sure user is the reservation owner
-        if(reservation.user.toString() !== req.user.id && req.user.role !== 'admin'){
-            return res.status(401).json({success:false, message:`User ${req.user.id} is not authorized to update this reservation`});
-        }
-
-        reservation = await Reservation.findByIdAndUpdate(req.params.id,req.body,{
-            new:true,
-            runValidators:true
-
-        });
-
-        res.status(200).json({success:true,data:reservation});
-
-    }catch(error){
-
-        console.log(error);
-        return res.status(500).json({success:false,message:"Cannot update Reservation"});
-    }
+    return true;
 };
 
 //@desc Delete reservation
@@ -132,24 +78,5 @@ exports.updateReservation = async (req,res,next)=>{
 //@access Private
 
 exports.deleteReservation = async (req,res,next) =>{
-    try{
-
-        const reservation = await Reservation.findById(req.params.id);
-        if(!reservation){
-            return res.status(404).json({success:false,message:`No reservation with the id of ${req.params.id}`});
-        }
-
-        //Make sure user is the reservation owner
-        if(reservation.user.toString() !== req.user.id && req.user.role !== 'admin'){
-            return res.status(401).json({success:false, message:`User ${req.user.id} is not authorized to update this reservation`});
-        }
-
-        await reservation.deleteOne();
-
-        res.status(200).json({success:true,data:{}});
-
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({success:false,message:"Cannot delete Reservation"});
-    }
+    return true;
 };
